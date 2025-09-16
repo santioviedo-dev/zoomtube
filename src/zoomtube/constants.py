a14180ff83a9af64798a5aa19919f9459ef19a85
santioviedo-dev <oviedos412@gmail.com>
2025-09-16 10:23:25 -0300

    add shared screen only type

--- src/zoomtube/constants.py
+++ src/zoomtube/constants.py
@@ -11,6 +11,7 @@ ZOOM_RECORDING_TYPES = [
 DEFAULT_PREFERRED_TYPES = [
     "shared_screen_with_speaker_view",
     "shared_screen_with_gallery_view",
+    "shared_screen_only",
     "gallery_view",
     "speaker_view",
 ]
